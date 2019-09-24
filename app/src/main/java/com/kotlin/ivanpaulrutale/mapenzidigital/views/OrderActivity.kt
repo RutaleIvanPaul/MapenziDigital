436c1ef8b95d34a1590158bc976f35c779dcb47b
RutaleIvanPaul <paulivan568@gmail.com>
2019-09-24 10:16:52 +0300

    Add social buttons and Order pages

--- app/src/main/java/com/kotlin/ivanpaulrutale/mapenzidigital/views/OrderActivity.kt
+++ app/src/main/java/com/kotlin/ivanpaulrutale/mapenzidigital/views/OrderActivity.kt
@@ -3,6 +3,8 @@ package com.kotlin.ivanpaulrutale.mapenzidigital.views
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.Spinner
 import com.kotlin.ivanpaulrutale.mapenzidigital.R
 
@@ -11,7 +13,26 @@ class OrderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
-        populateSpinner(findViewById(R.id.spinner),this)
+
+        val submit_order:Button = findViewById(R.id.submit_order)
+        val checkBox1:CheckBox= findViewById(R.id.checkBox1)
+        val checkBox2:CheckBox= findViewById(R.id.checkBox2)
+        val checkBox3:CheckBox= findViewById(R.id.checkBox3)
+        submit_order.setOnClickListener{
+            val shoppingCart = ArrayList<Int>()
+            val intent= Intent(this,PaymentActivity::class.java)
+            if (checkBox1.isChecked){
+                shoppingCart.add(1)
+            }
+            if (checkBox2.isChecked){
+                shoppingCart.add(2)
+            }
+            if (checkBox3.isChecked){
+                shoppingCart.add(3)
+            }
+            intent.putIntegerArrayListExtra("shoppingCart",shoppingCart)
+            startActivity(intent)
+        }
     }
 
     override fun onBackPressed() {
